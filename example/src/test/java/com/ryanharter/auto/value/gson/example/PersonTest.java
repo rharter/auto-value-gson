e4d7a2245f51bf2e4e85d25a4c4368c0e2bb8b81
Kevin Ménager <kevin@v-labs.fr>
2017-01-19 15:07:23 +0100

    update PersonTest

--- example/src/test/java/com/ryanharter/auto/value/gson/example/PersonTest.java
+++ example/src/test/java/com/ryanharter/auto/value/gson/example/PersonTest.java
@@ -2,12 +2,22 @@ package com.ryanharter.auto.value.gson.example;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class PersonTest {
     @Test
     public void testGson() throws Exception {
+        final DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+
+        String birthdate = "2007-11-11";
+        Date date = df.parse(birthdate);
+
+
         Gson gson = new GsonBuilder()
                 .registerTypeAdapterFactory(SampleAdapterFactory.create())
                 .create();
@@ -15,9 +25,10 @@ public class PersonTest {
                 .name("Piasy")
                 .gender(1)
                 .age(23)
+                .birthdate(date)
                 .address(Address.create("street", "city"))
                 .build();
-        String json = "{\"name\":\"Piasy\",\"gender\":1,\"age\":23,\"address\":{\"street-name\":\"street\",\"city\":\"city\"}}";
+        String json = "{\"name\":\"Piasy\",\"gender\":1,\"age\":23,\"birthdate\":\"" + birthdate + "\",\"address\":{\"street-name\":\"street\",\"city\":\"city\"}}";
 
         String toJson = gson.toJson(person, Person.class);
         Assert.assertEquals(json, toJson);
@@ -26,5 +37,6 @@ public class PersonTest {
         Assert.assertEquals("Piasy", fromJson.name());
         Assert.assertEquals(23, fromJson.age());
         Assert.assertEquals(1, fromJson.gender());
+        Assert.assertEquals(date, fromJson.birthdate());
     }
 }
