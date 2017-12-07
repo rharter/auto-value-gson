cfad58e772281e089461f64bac0491358ceee8ba
Zac Sweers <pandanomic@gmail.com>
2017-12-07 02:52:57 -0800

    Add generatetypeadater.FACTORY

--- auto-value-gson-annotations/src/main/java/com/ryanharter/auto/value/gson/GenerateTypeAdapter.java
+++ auto-value-gson-annotations/src/main/java/com/ryanharter/auto/value/gson/GenerateTypeAdapter.java
@@ -1,7 +1,16 @@
 package com.ryanharter.auto.value.gson;
 
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.reflect.TypeToken;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
@@ -11,7 +20,84 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  * TypeAdapter in a separate class rather than an inner class of an intermediate AutoValue-generated
  * class hierarchy.
  */
+@Inherited
 @Retention(RUNTIME)
 @Target(TYPE)
 public @interface GenerateTypeAdapter {
+
+  TypeAdapterFactory FACTORY = new TypeAdapterFactory() {
+    private final Map<Class<?>, Constructor<? extends TypeAdapter>> adapters =
+        new LinkedHashMap<>();
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
+      Class<? super T> rawType = type.getRawType();
+      if (!rawType.isAnnotationPresent(GenerateTypeAdapter.class)) {
+        return null;
+      }
+
+      Constructor<? extends TypeAdapter> constructor = findConstructorForClass(rawType);
+      if (constructor == null) {
+        return null;
+      }
+      //noinspection TryWithIdenticalCatches Resolves to API 19+ only type.
+      try {
+        if (constructor.getParameterCount() == 1) {
+          return constructor.newInstance(gson);
+        } else {
+          return constructor.newInstance(gson, type);
+        }
+      } catch (IllegalAccessException e) {
+        throw new RuntimeException("Unable to invoke " + constructor, e);
+      } catch (InstantiationException e) {
+        throw new RuntimeException("Unable to invoke " + constructor, e);
+      } catch (InvocationTargetException e) {
+        Throwable cause = e.getCause();
+        if (cause instanceof RuntimeException) {
+          throw (RuntimeException) cause;
+        }
+        if (cause instanceof Error) {
+          throw (Error) cause;
+        }
+        throw new RuntimeException(
+            "Could not create generated TypeAdapter instance for type " + rawType, cause);
+      }
+    }
+
+    private Constructor<? extends TypeAdapter> findConstructorForClass(Class<?> cls) {
+      Constructor<? extends TypeAdapter> adapterCtor = adapters.get(cls);
+      if (adapterCtor != null) {
+        return adapterCtor;
+      }
+      String clsName = cls.getName();
+      if (clsName.startsWith("android.")
+          || clsName.startsWith("java.")
+          || clsName.startsWith("kotlin.")) {
+        return null;
+      }
+      try {
+        Class<?> bindingClass = cls.getClassLoader()
+            .loadClass(clsName + "_GsonTypeAdapter");
+        try {
+          // Try the gson constructor
+          //noinspection unchecked
+          adapterCtor =
+              (Constructor<? extends TypeAdapter>) bindingClass.getConstructor(cls, Gson.class);
+        } catch (NoSuchMethodException e) {
+          // Try the gson + typetoken constructor
+          //noinspection unchecked
+          adapterCtor =
+              (Constructor<? extends TypeAdapter>) bindingClass.getConstructor(cls, Gson.class,
+                  TypeToken.class);
+        }
+      } catch (ClassNotFoundException e) {
+        adapterCtor = findConstructorForClass(cls.getSuperclass());
+      } catch (NoSuchMethodException e) {
+        throw new RuntimeException("Unable to find binding constructor for " + clsName, e);
+      }
+      adapters.put(cls, adapterCtor);
+      return adapterCtor;
+    }
+  };
 }
--- example/src/test/java/com/ryanharter/auto/value/gson/example/WebResponseTest.java
+++ example/src/test/java/com/ryanharter/auto/value/gson/example/WebResponseTest.java
@@ -3,6 +3,7 @@ package com.ryanharter.auto.value.gson.example;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import com.ryanharter.auto.value.gson.GenerateTypeAdapter;
 import java.lang.reflect.Type;
 import org.junit.Test;
 
@@ -32,6 +33,38 @@ public class WebResponseTest {
         "\"dataMap\":{\"key\":[{\"firstname\":\"Ryan\",\"lastname\":\"Harter\"}]}}";
     Gson gson = new GsonBuilder()
         .registerTypeAdapterFactory(SampleAdapterFactory.create())
+        .create();
+    Type responseType = new TypeToken<WebResponse<User>>(){}.getType();
+    WebResponse<User> response = gson.fromJson(json, responseType);
+
+    User expected = User.with("Ryan", "Harter");
+    assertEquals(expected, response.data());
+    assertEquals(expected, response.dataList().get(0));
+    assertEquals(expected, response.dataMap().get("key").get(0));
+  }
+
+  @Test public void handlesBasicTypesExternalAdapter() {
+    String json = "{\"status\":200,\"data\":\"string\"," +
+        "\"dataList\":[\"string\"]," +
+        "\"dataMap\":{\"key\":[\"string\"]}}";
+    Gson gson = new GsonBuilder()
+        .registerTypeAdapterFactory(GenerateTypeAdapter.FACTORY)
+        .create();
+    Type responseType = new TypeToken<WebResponse<String>>(){}.getType();
+    WebResponse<String> response = gson.fromJson(json, responseType);
+
+    assertEquals("string", response.data());
+    assertEquals("string", response.dataList().get(0));
+    assertEquals("string", response.dataMap().get("key").get(0));
+    assertEquals(200, response.status());
+  }
+
+  @Test public void handlesComplexTypesExternalAdapter() {
+    String json = "{\"status\":200,\"data\":{\"firstname\":\"Ryan\",\"lastname\":\"Harter\"}, " +
+        "\"dataList\":[{\"firstname\":\"Ryan\",\"lastname\":\"Harter\"}]," +
+        "\"dataMap\":{\"key\":[{\"firstname\":\"Ryan\",\"lastname\":\"Harter\"}]}}";
+    Gson gson = new GsonBuilder()
+        .registerTypeAdapterFactory(GenerateTypeAdapter.FACTORY)
         .create();
     Type responseType = new TypeToken<WebResponse<User>>(){}.getType();
     WebResponse<User> response = gson.fromJson(json, responseType);
