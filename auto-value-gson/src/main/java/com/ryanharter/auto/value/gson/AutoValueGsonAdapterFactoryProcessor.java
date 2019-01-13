dfa3db8f8b015ee727f890b7bfa30107fd39b1ca
Zac Sweers <pandanomic@gmail.com>
2019-01-13 02:38:57 -0800

    Allow no-arg static typeAdapter() methods
    Resolves #94

--- auto-value-gson/src/main/java/com/ryanharter/auto/value/gson/AutoValueGsonAdapterFactoryProcessor.java
+++ auto-value-gson/src/main/java/com/ryanharter/auto/value/gson/AutoValueGsonAdapterFactoryProcessor.java
@@ -169,7 +169,10 @@ public class AutoValueGsonAdapterFactoryProcessor extends AbstractProcessor {
       //noinspection ConstantConditions We've filtered absent ones
       ExecutableElement typeAdapterMethod = pair.second;
       List<? extends VariableElement> params = typeAdapterMethod.getParameters();
-      if (params != null && params.size() == 1) {
+      if (params == null || params.size() == 0) {
+        create.addStatement("return (TypeAdapter<$T>) $T." + typeAdapterMethod.getSimpleName() + "()", t,
+            elementType);
+      } else if (params.size() == 1) {
         create.addStatement("return (TypeAdapter<$T>) $T." + typeAdapterMethod.getSimpleName() + "($N)", t, elementType, gson);
       } else {
         create.addStatement("return (TypeAdapter<$T>) $T." + typeAdapterMethod.getSimpleName() + "($N, (($T) $N.getType()).getActualTypeArguments())",
