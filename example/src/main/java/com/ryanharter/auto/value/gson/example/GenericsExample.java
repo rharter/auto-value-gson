2ed0ccd5d775ab36a624639d65748ebb93c13929
Bryan Stern <bstern@slack-corp.com>
2019-10-16 11:37:33 -0700

    Fix factory method with generics #226

--- example/src/main/java/com/ryanharter/auto/value/gson/example/GenericsExample.java
+++ example/src/main/java/com/ryanharter/auto/value/gson/example/GenericsExample.java
@@ -11,6 +11,18 @@ import java.lang.reflect.Type;
   public abstract B b();
   public abstract C c();
 
+  @AutoValue.Builder
+  public interface Builder<A, B, C> {
+    Builder<A, B, C> a(A a);
+    Builder<A, B, C> b(B b);
+    Builder<A, B, C> c(C c);
+    GenericsExample<A, B, C> build();
+  }
+
+  public static <A, B, C> Builder<A, B, C> builder() {
+    return new AutoValue_GenericsExample.Builder<A, B, C>();
+  }
+
   public static <A, B, C> TypeAdapter<GenericsExample<A, B, C>> typeAdapter(Gson gson,
       Type[] types) {
     return new AutoValue_GenericsExample.GsonTypeAdapter<>(gson, types);
